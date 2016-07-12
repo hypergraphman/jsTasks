0240e11040d71435b98a0527c49a8725961717f3
hypergraphman <hypergraphman@gmail.com>
2016-07-12 23:30:08 +0300

    последний коммит на сегодня

--- Task04-ObjAndArr/splice.js
+++ Task04-ObjAndArr/splice.js
@@ -25,6 +25,7 @@ let splice = function () {
     for (let i = 0; i < lastArray.length; i++)
         arguments[0][i+start+insArray.length] = lastArray[i];
 
+    // если элементов стало меньше уменьшаем длину массива до нужной
     arguments[0].length = start+insArray.length+lastArray.length;
 
     return removed;
@@ -33,44 +34,44 @@ let splice = function () {
 
 module.exports = splice;
 
-var myFish = ['ангел', 'клоун', 'мандарин', 'хирург'];
-
-// удаляет 0 элементов с индекса 2 и вставляет элемент 'барабанщик'
- var removed = splice(myFish, 2, 0, 'барабанщик');
-// myFish равен ['ангел', 'клоун', 'барабанщик', 'мандарин', 'хирург']
-// removed равен [], никакие элементы не были удалены
-
-console.log(myFish);
-console.log(removed);
-
-// удаляет 1 элемент с индекса 3
-removed = splice(myFish, 3, 1);
-// myFish равен ['ангел', 'клоун', 'барабанщик', 'хирург']
-// removed равен ['мандарин']
-
-console.log(myFish);
-console.log(removed);
-
-// удаляет 1 элемент с индекса 2 и вставляет элемент 'телескоп'
-removed = splice(myFish, 2, 1, 'телескоп');
-// myFish равен ['ангел', 'клоун', 'телескоп', 'хирург']
-// removed равен ['барабанщик']
-
-console.log(myFish);
-console.log(removed);
-
-// удаляет 2 элемента с индекса 0 и вставляет элементы 'попугай', 'анемон' и 'голубая'
-removed = splice(myFish, 0, 2, 'попугай', 'анемон', 'голубая');
-// myFish равен ['попугай', 'анемон', 'голубая', 'телескоп', 'хирург']
-// removed равен ['ангел', 'клоун']
-
-console.log(myFish);
-console.log(removed);
-
-// удаляет 2 элемента с индекса 3
-removed = splice(myFish, 3, Number.MAX_VALUE);
-// myFish равен ['попугай', 'анемон', 'голубая']
-// removed равен ['телескоп', 'хирург']
-
-console.log(myFish);
-console.log(removed);
+// var myFish = ['ангел', 'клоун', 'мандарин', 'хирург'];
+//
+// // удаляет 0 элементов с индекса 2 и вставляет элемент 'барабанщик'
+//  var removed = splice(myFish, 2, 0, 'барабанщик');
+// // myFish равен ['ангел', 'клоун', 'барабанщик', 'мандарин', 'хирург']
+// // removed равен [], никакие элементы не были удалены
+//
+// console.log(myFish);
+// console.log(removed);
+//
+// // удаляет 1 элемент с индекса 3
+// removed = splice(myFish, 3, 1);
+// // myFish равен ['ангел', 'клоун', 'барабанщик', 'хирург']
+// // removed равен ['мандарин']
+//
+// console.log(myFish);
+// console.log(removed);
+//
+// // удаляет 1 элемент с индекса 2 и вставляет элемент 'телескоп'
+// removed = splice(myFish, 2, 1, 'телескоп');
+// // myFish равен ['ангел', 'клоун', 'телескоп', 'хирург']
+// // removed равен ['барабанщик']
+//
+// console.log(myFish);
+// console.log(removed);
+//
+// // удаляет 2 элемента с индекса 0 и вставляет элементы 'попугай', 'анемон' и 'голубая'
+// removed = splice(myFish, 0, 2, 'попугай', 'анемон', 'голубая');
+// // myFish равен ['попугай', 'анемон', 'голубая', 'телескоп', 'хирург']
+// // removed равен ['ангел', 'клоун']
+//
+// console.log(myFish);
+// console.log(removed);
+//
+// // удаляет 2 элемента с индекса 3
+// removed = splice(myFish, 3, Number.MAX_VALUE);
+// // myFish равен ['попугай', 'анемон', 'голубая']
+// // removed равен ['телескоп', 'хирург']
+//
+// console.log(myFish);
+// console.log(removed);
